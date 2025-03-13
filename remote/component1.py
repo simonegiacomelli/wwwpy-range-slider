102f9d9639e4f249819e66a418e325947cc69838
simone <simone.giacomelli@gmail.com>
2025-03-13 17:21:02 +0000

    initial commit

---
+++ remote/component1.py
@@ -0,0 +1,65 @@
+import wwwpy.remote.component as wpc
+import js
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Component1(wpc.Component, tag_name='component-1'):
+
+    def init_component(self):
+        # language=html
+        self.element.innerHTML = """
+<div>component-1</div>
+
+<style>
+.slidecontainer {
+  width: 100%;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 25px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  background: #04AA6D;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  background: #04AA6D;
+  cursor: pointer;
+}
+</style>
+
+
+
+<h1>Custom Range Slider</h1>
+
+<div class="slidecontainer">
+  <p>Default range slider:</p>
+  <input type="range" min="1" max="100" value="50">
+  
+  <p>Custom range slider:</p>
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+
+"""
